19c23dfea3dbee5179b656c2c4b719f7ac80b273
Vadim Tikhanoff <vtikha@gmail.com>
2021-05-19 11:54:59 +0200

    adding thrift file

--- speechInteraction/modules/googleSynthesis/googleSynthesis.thrift
+++ speechInteraction/modules/googleSynthesis/googleSynthesis.thrift
@@ -24,4 +24,57 @@ service googleSynthesis_IDL
      */
     bool quit();
 
+    /**
+     * Say the phrase.
+     * @param string containing the phrase to synthesise
+     * @return true/false on success/failure
+     */
+    bool say(1:string phrase);
+
+    /**
+     * Set the language of the synthesiser. 
+     * example: setLanguage en-US en-US-Wavenet-A
+     * @param string containing the languageCode
+     * @param string containing the voiceCode
+     * @return true/false on success/failure
+     */
+    string setLanguage(1:string languageCode, 2:string voiceCode);
+
+    /**
+     * Set the pitch of the synthesiser.
+     * @param double containing the desired pitch for the synthesiser
+     * @return true/false on success/failure
+     */
+    bool setPitch(1:double pitch);
+
+    /**
+     * Set the speed of the synthesiser.
+     * @param double containing the desired speed for the synthesiser
+     * @return true/false on success/failure
+     */
+    bool setSpeed(1:double speed);
+
+    /**
+     * Get the language code of the synthesiser
+     * @return string containing the language code
+     */
+    string getLanguageCode();
+
+     /**
+     * Get the voice code of the synthesiser
+     * @return string containing the voice code
+     */
+    string getVoiceCode();
+
+     /**
+     * Get the pitch value of the synthesiser
+     * @return string containing the pitch value
+     */
+    double getPitch();
+
+    /**
+     * Get the speed value of the synthesiser
+     * @return string containing the speed code
+     */
+    double getSpeed();
 }
